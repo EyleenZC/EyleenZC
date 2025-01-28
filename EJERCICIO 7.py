13b49102194971fd9b31d4b3f8a94ad16c8aee2f
EyleenZC <eyleenzambrano367@gmail.com>
2025-01-28 12:06:09 -0500

    Delete EJERCICIO 7.py

--- EJERCICIO 7.py
+++
@@ -1,8 +0,0 @@
-# Solicitar un número entero al usuario
-numero = int(input("Ingresa un número entero: "))
-
-# Determinar si es par o impar
-if numero % 2 == 0:
-    print(f"El número {numero} es par.")
-else:
-    print(f"El número {numero} es impar.")
